66f262be7e3272050a4e5986477a79b81f82e5b4
robert2021south <robert2021south@gmail.com>
2026-03-03 00:32:12 +0800

    删除不存在的类文件，删除了

--- inc/loader.php
+++ inc/loader.php
@@ -5,9 +5,6 @@
 // 只在需要时加载 UsersWP 相关功能
 if (defined('USERSWP_VERSION') || class_exists('UsersWP')) {
     require_once ASTRA_CHILD_PATH . '/inc/userswp/class-account-tabs.php';
-    require_once ASTRA_CHILD_PATH . '/inc/userswp/class-api-client.php';
-    require_once ASTRA_CHILD_PATH . '/inc/userswp/class-license-handler.php';
-    require_once ASTRA_CHILD_PATH . '/inc/userswp/class-order-handler.php';
     // 初始化
     new RW_Account_Tabs();
 }
